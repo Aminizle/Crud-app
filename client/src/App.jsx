6a02b6662c3dba6442dc0d2576e2d8b4cee0114f
Ameen Mohiyuddin <57292718+Aminizle@users.noreply.github.com>
2023-07-16 20:23:14 -0400

    added routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,23 @@
 import { useState } from 'react'
-
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
+import Users from './Users'
+import CreateUsers from './CreateUser'
+import UpdateUsers from './UpdateUser'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Users />}></Route>
+          <Route path='/create' element={<CreateUsers />}></Route>
+          <Route path='/update' element={<UpdateUsers />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ client/src/CreateUser.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function CreateUsers() {
+  return <div>Create Users</div>;
+}
+
+
+export default CreateUsers;
