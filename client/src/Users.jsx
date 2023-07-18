b103f40fbd0818dfb609be4eae528e9e9b12c1a6
Ameen Mohiyuddin <57292718+Aminizle@users.noreply.github.com>
2023-07-17 20:19:30 -0400

    added data display from mongoDB

--- client/src/Users.jsx
+++ client/src/Users.jsx
@@ -1,14 +1,17 @@
-import React, {useState} from "react";
+import axios from "axios"
+import React, {useEffect, useState} from "react";
 import { Link } from "react-router-dom";
 
 function Users() {
-  const [users, setUsers] = useState([
-    {
-      Name: "Yousaf",
-      Email: "ysf@gmail.com",
-      Age: 20,
-    }
-  ]);
+  const [users, setUsers] = useState([]);
+
+  useEffect(()=> {
+    axios.get('http://localhost:3001')
+    .then(result => setUsers(result.data))
+    .catch(err => console.log(err))
+  }, [])
+
+
   return (
     <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
       <div className="w-50 bg-white rounded p-3">
@@ -25,12 +28,12 @@ function Users() {
           <tbody>
             {users.map((user) => {
                 return <tr>
-                <td>{user.Name}</td>
-                <td>{user.Email}</td>
-                <td>{user.Age}</td>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.age}</td>
                 <td>
                 <Link to="/update" className="btn btn-success">Update</Link>
-                  <button>Delete</button>
+                  <button className="btn btn-danger">Delete</button>
                 </td>
               </tr>;
             })}
